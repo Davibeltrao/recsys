81ef20e13bf0517e4613f3fd34efc55752c83708
brenocg29 <gabrichbreno@gmail.com>
2018-06-10 17:37:31 -0300

    aa

--- bleh.py
+++ bleh.py
@@ -20,10 +20,12 @@ userpay = {}
 usergeneral = {}
 ratings = {}
 bestRest = {}
-
-
+formPrice = 0.0
+total = 0.0
 def GetPredictedFeature(forcollab,userId):
 	predictedFeatures = {}
+	global formPrice
+	global total
 	characteristics = {"smoker": 2, "drink_level": 3,"dress_pref":4,"ambience":5,"Transport":6, "Marital":7, "filhos":8,"interest":10, "personality":11, "religion":12,"activity":13,"budget":16}
 	largest = 0
 	largestId = ""
@@ -60,6 +62,8 @@ def GetPredictedFeature(forcollab,userId):
 					bestRest[i] = []
 				else:
 					bestRest[i].append(a["itemId"])
+	if userId not in bestRest:
+		return 
 	for qw in bestRest[userId]:
 		if qw in accepts:
 			for i in accepts[qw]:
@@ -115,6 +119,10 @@ def GetPredictedFeature(forcollab,userId):
 	#else:
 	#	predictedFeatures = {"smoker": "false" if greatestValues["smoking"] == "none" else "true", "drink_level": usergeneral[u][4],"dress_pref": usergeneral[u][5],"ambience": usergeneral[u][6],"Transport": usergeneral[u][7], "Marital": usergeneral[u][8], "filhos": usergeneral[u][9],"interest": usergeneral[u][11], "personality": usergeneral[u][12], "religion": usergeneral[u][13],"activity":usergeneral[u][14],"budget": usergeneral[u][17]}
 	print predictedFeatures
+	if greatestValues["dress_pref"][0] == "informal" and (greatestValues["price"][0] =="medium" or greatestValues["price"][0] == "low"):
+		formPrice += 1
+	if greatestValues["dress_pref"][0] == "informal":
+		total += 1
 def UserSimilarity(userVec,mostSimilarVec):
 	#we can weight each data and use the two weighted info(from te restaurants user like and from users that a similar sometimes user similarity is way more accurate than user preferences)
 	confiability = 0.0
@@ -137,9 +145,13 @@ def getBiggest(mapId):
 		if mapId[i] > greatest:
 			greatest = mapId[i]
 			greatestId = i
+	if cont == 0:
+		return ("dummy",0)
 	return (greatestId,greatest/cont)
 
 def similarity(x,y):
+	if x == y:
+		return 0
 	acumup = 0
 	acumdown = 0
 	A = 0
@@ -261,4 +273,6 @@ print "FM MSE: %.4f" % math.sqrt(mean_squared_error(ratingsTest,preds))
 vectorizer = DictVectorizer()
 X = vectorizer.fit_transform(forcollab["U1003"])
 Y = vectorizer.transform(forcollab["U1003"])
-GetPredictedFeature(forcollab,"U1003")
+for i in forcollab:
+	GetPredictedFeature(forcollab,i)
+print formPrice/total
